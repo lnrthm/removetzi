ed613e6ec0412b9fd57d6ae43516827957396449
lnrthm <hallo@lnrt.de>
2017-03-20 21:05:58 +0100

    Create removetzi.user.js

---
+++ removetzi.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        AutoClicktaz
+// @include     http://taz.de
+// @include     https://taz.de
+// @include     http://www.taz.de
+// @include     https://www.taz.de
+// @grant       GM_addStyle
+// ==/UserScript==
+
+GM_addStyle ("div#tzi-paywahl-fg, div#tzi-paywahl-bg {display:none;}")
