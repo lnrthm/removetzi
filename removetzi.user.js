0cf7893aaf854072f4ead80bbd24138eb8adca07
lnrthm <hallo@lnrt.de>
2017-03-20 21:22:14 +0100

    Update removetzi.user.js

--- removetzi.user.js
+++ removetzi.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        removetzi
-// @include     http://taz.de
-// @include     https://taz.de
-// @include     http://www.taz.de
-// @include     https://www.taz.de
+// @include     http://taz.de/*
+// @include     https://taz.de/*
+// @include     http://www.taz.de/*
+// @include     https://www.taz.de/*
 // @grant       GM_addStyle
 // ==/UserScript==
 
